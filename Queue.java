13e278c93769b698055b7787c09841cd95aed0b2
priyusha99 <sunkarapriyusha1999@gmail.com>
2021-12-05 12:01:55 +0530

    Implementation of enqueue()

--- Queue.java
+++ Queue.java
@@ -5,15 +5,21 @@ import java.io.*;
 import java.util.*;
 
 class Queue {
-
-
+    private Stack<Integer> stack1;
+    private Stack<Integer> stack2;
     public Queue() {
-
+        stack1=new Stack<Integer> ();
+        stack1=new Stack<Integer> ();
     }
 
     // Push element x to the back of queue.
+    //Time Complexity: O(1), Space Complexity: O(N)
     public void enqueue(int x) {
-
+        while(!stack1.isEmpty())
+        stack2.push(stack1.pop());
+        stack2.push(x);
+        while(!stack2.isEmpty())
+        stack1.push(stack2.pop());
     }
 
     // Removes the element from in front of queue.
