7d324b9c9dc75e0e466b0e6ca094aa8451583d5d
priyusha99 <sunkarapriyusha1999@gmail.com>
2021-12-05 12:12:42 +0530

    implementation for empty()

--- Queue.java
+++ Queue.java
@@ -35,8 +35,9 @@ class Queue {
     }
     
     // Return whether the queue is empty.
+    //Time Complexity: O(1), Space Complexity: 0
     public boolean empty() {
-
+        return stack1.isEmpty() && stack2.isEmpty();
     }
 
     // Return the number of elements in queue.
